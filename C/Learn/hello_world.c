4d9d385af2b94768c6ea44ded07e642e37cf10af
ratri0git <ratri0nil@gmail.com>
2025-07-31 00:21:45 +0530

    learning operations and operators

--- C/Learn/hello_world.c
+++ C/Learn/hello_world.c
@@ -1,6 +1,24 @@
 #include<stdio.h>
 
 void main(){
-    printf("Hello World !");
+    printf("Hello World ! \n");
     
+    int age = 22;
+    float rating = 4.5;
+    char name = 'N';
+
+    printf("age is %d \n",age);         // \n gives newline 
+    printf("name is %c \n",name);
+    printf("rating is %f \n",rating);
+
+    /* 
+    printf print data
+    scanf read data from keyboard
+    */
+
+    int a,b;
+    scanf("%d",&a);
+    scanf("%d",&b);
+    printf("sum is : %d",a+b);
+
 }
---
+++ C/Learn/instructions_operators.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+void main(){
+    int a=1;
+    int b=a;
+    int c=b*5;
+    
+    int d=1,e;
+
+    int x,y,z;
+    x=y=z=1;
+}
