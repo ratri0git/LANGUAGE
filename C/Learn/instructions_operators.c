6982d4e83ede6cab34a6489e109d3e082b0bf572
ratri0git <ratri0nil@gmail.com>
2025-07-31 01:22:29 +0530

    done

--- C/Learn/instructions_operators.c
+++ C/Learn/instructions_operators.c
@@ -1,12 +1,23 @@
 #include<stdio.h>
+#include<math.h>
 
-void main(){
-    int a=1;
-    int b=a;
-    int c=b*5;
+
+int main(){
     
     int d=1,e;
 
     int x,y,z;
     x=y=z=1;
+
+
+// Type conversion implicit
+    printf("division is : %d \n",b/a);
+    printf("division is : %f \n ",b/f);
+
+// Type conversion explicit
+    a= (int) 1.9;
+    printf("%d \n",a);
+
+    return 0;
+
 }
