41ca51360242674a4cbcc0126eaa926a5ff123cf
ratri0git <ratri0nil@gmail.com>
2025-08-06 16:35:06 +0530

    done

---
+++ C/Exercise_1/q15.c
@@ -0,0 +1,21 @@
+/*
+Write a program in C to print all the letters in english alphabet using a pointer
+*/
+
+#include<stdio.h>
+
+int main(){
+
+    char alphabet= 'a';
+    char *ptr = &alphabet;
+
+    for(int i=0; i<26;i++){
+        printf("%c\t",*ptr);
+        (*ptr)++;
+    }
+
+    return 0;
+}
+
+
+
