5fa5894106e826a577bc91a8a830e41d20ea18bb
ratri0git <ratri0nil@gmail.com>
2025-08-07 17:10:29 +0530

    done

---
+++ PYthon/Learn/variables_datatypes.py
@@ -0,0 +1,36 @@
+
+name = "Nil"
+age = 22
+rating = 4.5
+
+print("My name is : ",name)
+print("my age is : ",age)
+print("I got ",rating,"stars")
+
+print(type(name))
+print(type(age))
+print(type(rating))
+
+print('name')
+print("name")
+print('''name''')
+
+print(True)
+print(False)
+print(None)
+
+num1=12
+num2=13
+sum=num1+num2
+diff=num1-num2
+print(sum)
+print(diff)
+
+# end='' clear leftover newline
+print("\nTHE END",end='')
+
+"""
+This
+is a multi line 
+comment
+"""
