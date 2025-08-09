e25205a55d290676b077f535bb8f57f199d54e4c
ratri0git <ratri0nil@gmail.com>
2025-08-10 01:31:41 +0530

    done

---
+++ PYthon/Project/p5.py
@@ -0,0 +1,28 @@
+# This program generates QR code
+
+'''
+# Install set-up and activate and deactivate in virtual environment on terminal
+
+sudo apt install python3-venv -y
+python3 -m venv qr-env
+source qr-env/bin/activate
+pip install qrcode[pil]
+
+deactivate
+
+'''
+import qrcode
+
+data = input("Enter URL : ").strip() # remove whitespaces with strip
+
+file_name = input("Enter file name : ").strip()
+
+qr = qrcode.QRCode(box_size=10, border=4)
+qr.add_data(data)
+qr.make(fit=True)
+image = qr.make_image(fill_color='black', back_color='white')
+image.save(file_name)
+print("Done")
+
+
+print("END",end='')
