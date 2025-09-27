17fda6b3fbc224f65cbb8705de9bc68d1280c0fe
ratri0git <ratri0nil@gmail.com>
2025-09-27 19:03:09 +0530

    done

---
+++ PYthon/Learn/mod1.py
@@ -0,0 +1,8 @@
+def addition(a,b):
+    return a+b
+
+def multiplication(a,b):
+    return a*b
+
+print(addition(2,5))
+print(multiplication(2,5))
---
+++ PYthon/Learn/package_module.py
@@ -0,0 +1,3 @@
+import mod1
+
+print(mod1.addition(4,5))
