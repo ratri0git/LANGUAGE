61e01ae6a7d507dcc065724dfbc5124bde2ad3ac
ratri0git <ratri0nil@gmail.com>
2025-09-27 20:14:26 +0530

    done

--- baSH/Project/p1.sh
+++ baSH/Project/p1.sh
@@ -1,28 +1,31 @@
-#!/bin/bash
-
 # write a script to throw dice and give user id if any die output is 6
 
 
+#!/bin/bash
+
 read -p "Enter number of dice to use : " num
 
-dice_out=0
-i=1
-while [[ $i -le $num ]]
+until [[ $try_again == 0 ]]
 do
-    let die_out=$RANDOM%6+1
-    echo "Output of die $i: $die_out"
-    let dice_out=$die_out+$dice_out
-    let i++
+    i=1
+    try_again=1
 
-    if [[ $die_out -eq 6 ]]
-    then
-        echo "------------------------"
-        echo "unlocked user id : $UID"
-        echo "------------------------"
+    while [[ $i -le $num ]]
+    do
+        let die_out=$RANDOM%6+1
+        echo "Output of die $i: $die_out"
+        let i++
 
-    fi
+        if [[ $die_out -eq 6 ]]
+        then
+            echo "------------------------"
+            echo "unlocked user id : $UID"
+            echo "------------------------"
+
+        fi
+
+    done
+
+    read -p "Press 0 to quit or try again : " try_again
 
 done
-
-echo "Dice output : $dice_out"
-
